a9bce71fea7bcdc5e14dc2c834ec8807217f97e5
harshay <harshaykr@hotmail.com>
2020-03-09 20:29:26 +0000

    update

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+
+class game { 
+
+    constructor() {
+
+        this.missed       = 0;
+        this.phrases      = [];
+        this.activePhrase = null;
+
+    }
+    
+}; 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+/*
+ const phrase = new phrase();
+ const game   = new game();
+ */
+
+const phrase = new Phrase('Life is like a box of chocolates'); 
+console.log(`Phrase - phrase: ${phrase.phrase}`); 
