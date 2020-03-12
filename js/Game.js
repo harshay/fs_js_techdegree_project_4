bb411b45ade7c29154b32705b4e78de987cdb3aa
harshay <harshaykr@hotmail.com>
2020-03-12 06:25:35 +0000

    update

--- js/Game.js
+++ js/Game.js
@@ -2,15 +2,33 @@
  * Project 4 - OOP Game App
  * Game.js */
 
+/*
+//phrases
+
+deprive
+multiply
+target
+solve
+await
+
+*/
 
 class game { 
 
     constructor() {
 
         this.missed       = 0;
-        this.phrases      = [];
+        this.phrases      = createPhrases();
         this.activePhrase = null;
 
     }
+
+    createPhrases() {
+
+        all_phrases = ['deprive','multiply','target','solve','await'];
+
+        return all_phrases;
+
+    }; 
     
 }; 
--- js/app.js
+++ js/app.js
@@ -7,5 +7,11 @@
  const game   = new game();
  */
 
+ /*
 const phrase1 = new phrase('Life is like a box of chocolates'); 
-console.log(`Phrase - phrase: ${phrase1.phrase1}`); 
+console.log(`Phrase - phrase: ${phrase1.phrase}`); 
+*/
+
+const game1 = new game(); 
+ game1.phrases.forEach((phrase, index) => {   console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);  }); 
+  
