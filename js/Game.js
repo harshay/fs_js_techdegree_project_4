3106d3494e88805eb9209432b0c4844508cfb411
harshay <harshaykr@hotmail.com>
2020-03-30 22:56:21 +0100

    update

--- js/Game.js
+++ js/Game.js
@@ -13,8 +13,15 @@ await
 
 */
 
+//.chosen
+//.wrong
+
+
+//always use this when calling a method or using a property within the same class (unless var is local to the mehod)
 class Game { 
 
+
+    //creates properties that can be accessed by new objects of this class or methods in this class
     constructor() {
 
         this.missed       = 0;
@@ -24,6 +31,7 @@ class Game {
 
     }
 
+     //creates an array of phrase objects 
     createPhrases() {
 
         let all_phrases = 
@@ -49,13 +57,16 @@ class Game {
 
     };
 
+
     startGame() {
 
         let mainCover = document.getElementById("overlay");
         mainCover.style.display = "none";
 
+        //returs a random object
         let selectedPhraseObj =  this.getRandomPhrase();
         
+        //runs the aptd method 
         selectedPhraseObj.addPhraseToDisplay(); 
         
         this.activePhrase = selectedPhraseObj;
@@ -71,6 +82,10 @@ class Game {
 
     handleInteraction(button){ 
 
+        //new phrase object gives access to all its methods
+        //runs the check letter object part of the phrase class
+        //button parameter within the handle interaction method is being passed a value by calling it in app.js 
+        //the parameter value passed can now be picked up by the methods run in the phrase class
         this.activePhrase.checkLetter(button.innerHTML); 
 
         this.activePhrase.showMatchedLetter(button.innerHTML); 
@@ -83,7 +98,22 @@ class Game {
 
         let gameWon; 
 
-        if(this.correct ===  this.activePhrase.phraseSplit.length) {
+
+        //remove duplicates from this.activePhrase.phraseSplit.length
+        //indexof searches the array for the specific item and returns its index position
+        //all array items passed one by one in the filter/index of combo
+        //when === index specified the matching array item will only pass once because when repeated the index wont match
+        let prePhraseDeDup = this.activePhrase.phraseSplit;
+
+        let PhraseDeDuped = prePhraseDeDup.filter(function (phr, index) {
+        
+            return prePhraseDeDup.indexOf(phr) === index;
+        
+        
+        });
+
+
+        if(this.correct ===  PhraseDeDuped.length) {
 
             gameWon = true;
 
@@ -122,9 +152,15 @@ class Game {
 
             imgListEntry.appendChild(img);
 
+            button.className = "wrong";
+
         } else {
 
             this.correct += 1;
+
+            button.disabled = true;
+
+            button.className = "chosen";
 
             console.log(this.correct);
 
--- js/app.js
+++ js/app.js
@@ -2,20 +2,6 @@
  * Project 4 - OOP Game App
  * app.js */
 
-
-/*
-const game = new Game(); 
-const randomPhrase = game.getRandomPhrase(); 
-const phrase = new Phrase(randomPhrase.phrase); 
-phrase.addPhraseToDisplay(); 
- */
-
-/*
-const game = new Game(); 
-game.startGame(); 
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
-*/
-
 let game;
 
 let startGameBtn = document.getElementById("btn__reset");
@@ -24,33 +10,48 @@ let keyboardLength = keyboard.length;
 
 //problems to be fixed :
 //game doesnt reset
-//letter already guessed corretly increments correct guesses when the phrase has repeating charaters
+//letter  guessed with repeating charaters breaks the check for win method
 //selecting the correct character already guessed again increments the correct count
-
 
 
 startGameBtn.addEventListener("click",() => {
 
+    // creates a new game object, gives access to all its properties and methods
     game = new Game(); 
 
     game.startGame();    
-    
+
+    //funtion will run two methods within the newly created game object
+    let hirl = (event) => {
+
+         //runs the handlintr method pass the event.target value as an input
+         game.handleInteraction(event.target);
+         //runs the removeLife method pass the event.target value as an input
+         game.removeLife(event.target);
+
+    };
+     
         //move to app.js
         for(let i = 0; i < keyboardLength; i += 1) {
 
 
+            //keys with 'click' event listener
             keyboard[i].addEventListener("click",(event) => {
 
-
-                game.handleInteraction(event.target);
-
-                game.removeLife(event.target);
+                hirl(event);
                 
                 
             });
 
-        };
-        
+            //keys with 'keydown' event listener (keyboard button press)
+            keyboard[i].addEventListener("keyup",(event) => {
+
+                hirl(event);
+                
+                
+            });
+
+        };        
 
 });
 
